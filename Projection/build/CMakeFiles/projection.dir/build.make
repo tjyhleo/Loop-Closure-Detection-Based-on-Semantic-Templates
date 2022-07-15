1d6d86b0022f14da2ab017b018663c828449094b
Jialin <tjyhleo@outlook.com>
2022-07-15 16:25:25 +0800

    projection half done

--- Projection/build/CMakeFiles/projection.dir/build.make
+++ Projection/build/CMakeFiles/projection.dir/build.make
@@ -60,15 +60,15 @@ include CMakeFiles/projection.dir/flags.make
 CMakeFiles/projection.dir/projection.cpp.o: CMakeFiles/projection.dir/flags.make
 CMakeFiles/projection.dir/projection.cpp.o: ../projection.cpp
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green --progress-dir=/home/jialin/Documents/VSC_Projects/Projection/build/CMakeFiles --progress-num=$(CMAKE_PROGRESS_1) "Building CXX object CMakeFiles/projection.dir/projection.cpp.o"
-	/usr/bin/g++  $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -o CMakeFiles/projection.dir/projection.cpp.o -c /home/jialin/Documents/VSC_Projects/Projection/projection.cpp
+	/usr/bin/c++  $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -o CMakeFiles/projection.dir/projection.cpp.o -c /home/jialin/Documents/VSC_Projects/Projection/projection.cpp
 
 CMakeFiles/projection.dir/projection.cpp.i: cmake_force
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green "Preprocessing CXX source to CMakeFiles/projection.dir/projection.cpp.i"
-	/usr/bin/g++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -E /home/jialin/Documents/VSC_Projects/Projection/projection.cpp > CMakeFiles/projection.dir/projection.cpp.i
+	/usr/bin/c++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -E /home/jialin/Documents/VSC_Projects/Projection/projection.cpp > CMakeFiles/projection.dir/projection.cpp.i
 
 CMakeFiles/projection.dir/projection.cpp.s: cmake_force
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green "Compiling CXX source to assembly CMakeFiles/projection.dir/projection.cpp.s"
-	/usr/bin/g++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -S /home/jialin/Documents/VSC_Projects/Projection/projection.cpp -o CMakeFiles/projection.dir/projection.cpp.s
+	/usr/bin/c++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -S /home/jialin/Documents/VSC_Projects/Projection/projection.cpp -o CMakeFiles/projection.dir/projection.cpp.s
 
 CMakeFiles/projection.dir/projection.cpp.o.requires:
 
--- Projection/projection.cpp
+++ Projection/projection.cpp
@@ -9,13 +9,16 @@ int main(){
     float f;
     double d;
     ifstream inFile;
+    
+    ////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////intrinsic matrix///////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
     inFile.open("/media/jialin/045E58135E57FC3C/UBUNTU/KITTI360/calibration/perspective.txt");
     if(!inFile){
         cout<<"unable to open file" <<endl;
         exit(1);
     }
 
-    // vector<string> strVec;
     string::size_type idx;
     string kw="P_rect_00:";
     string target_string;
@@ -30,39 +33,173 @@ int main(){
         }
     }
 
+    
+    vector<double> intrin_vec;
     stringstream ss(target_string);
     while(ss>>d){
-        cout<<d<<endl;
+        intrin_vec.push_back(d);
     }
 
-
-    std::cout<<target_string<<std::endl;
+    Mat intrinsic_Mat(intrin_vec);
+    intrinsic_Mat = intrinsic_Mat.reshape(1,(3,4));
+    // cout <<intrinsic_Mat.size<<endl;
+    // Mat Intrinsic_Mat = intrinsic_Mat.colRange(1,3).clone();
+    Mat Intrinsic_Mat = Mat::zeros(3,3,CV_64F);
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            Intrinsic_Mat.at<double>(i,j) = intrinsic_Mat.at<double>(i,j);
+        }
+    }
+    cout << Intrinsic_Mat << endl;
 
     inFile.close();
 
-    // vector<Point3f> cloudPoints;
-    // cloudPoints.push_back((1196.659790039062,-1735.256347656250,143.373703002930));
-    // double cloudPoints[3][3]={
-    //     {1196.659790039062,-1735.256347656250,143.373703002930},
-    //     {1198.843627929688,-1741.103149414062,141.547698974609},
-    //     {1195.647460937500,-1749.011230468750,140.955093383789}
-    // };
 
-    // double intrinsicMatrix[3][3]={
-    //     {552.554261, 0.000000, 682.049453},
-    //     {0.000000, 552.554261, 238.769549},
-    //     {0.000000, 0.000000, 1.000000}
-    // };
+    ////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////camera_to_pose matrix///////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    inFile.open("/media/jialin/045E58135E57FC3C/UBUNTU/KITTI360/calibration/calib_cam_to_pose.txt");
+    if(!inFile){
+        cout<<"unable to open file" <<endl;
+        exit(1);
+    }
+
+    // string::size_type idx2;
+    kw="image_00:";
+    // string target_string;
+    while(!inFile.eof()){
+        string inLine;
+        getline(inFile, inLine,'\n');
+        // strVec.push_back(inLine);
+        idx=inLine.find(kw);
+        if(idx!=string::npos){
+            target_string=inLine.substr(10);
+            break;
+        }
+    }
+    target_string += "0 0 0 1"; 
+    // cout<<target_string<<endl;
+    vector<double> cam2pose_vec;
+    // stringstream ss(target_string);
+    ss.clear();
+    ss.str(target_string);
+    while(ss>>d){
+        cam2pose_vec.push_back(d);
+    }
+
+    // cout<<cam2pose_vec[-1]<<endl;
+    Mat cam2pose_Mat(cam2pose_vec);
+    cam2pose_Mat = cam2pose_Mat.reshape(1,(4,4));
+    cout << cam2pose_Mat << endl;
+
+    inFile.close();
+
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////////rectification_matrix//////////////////
+    //////////////////////////////////////////////////////////////////////
+    inFile.open("/media/jialin/045E58135E57FC3C/UBUNTU/KITTI360/calibration/perspective.txt");
+    if(!inFile){
+        cout<<"unable to open file" <<endl;
+        exit(1);
+    }
+
+    // string::size_type idx2;
+    kw="R_rect_00:";
+    // string target_string;
+    while(!inFile.eof()){
+        string inLine;
+        getline(inFile, inLine,'\n');
+        // strVec.push_back(inLine);
+        idx=inLine.find(kw);
+        if(idx!=string::npos){
+            target_string=inLine.substr(11);
+            break;
+        }
+    }
+    // cout<<target_string<<endl;
+    vector<double> rect_vec;
+    // stringstream ss(target_string);
+    ss.clear();
+    ss.str(target_string);
+    while(ss>>d){
+        rect_vec.push_back(d);
+    }
+
+    // cout<<cam2pose_vec.size()<<endl;
+    Mat rect_Mat(rect_vec);
+    rect_Mat = rect_Mat.reshape(1,(3,3));
+
+    Mat Rect_Mat = Mat::eye(4,4,CV_64F);
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            Rect_Mat.at<double>(i,j) = rect_Mat.at<double>(i,j);
+        }
+    }
+    cout << Rect_Mat << endl;
+
+    inFile.close();
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////pose//////////////////////////////////////
+    inFile.open("/media/jialin/045E58135E57FC3C/UBUNTU/KITTI360/data_poses/2013_05_28_drive_0007_sync/poses.txt");
+    if(!inFile){
+        cout<<"unable to open file" <<endl;
+        exit(1);
+    }
+
+    // string::size_type idx2;
+    kw="1";
+    // string target_string;
+    while(!inFile.eof()){
+        string inLine;
+        getline(inFile, inLine,'\n');
+        // strVec.push_back(inLine);
+        idx=inLine.find(kw);
+        if(idx!=string::npos){
+            target_string=inLine.substr(2);
+            break;
+        }
+    }
+    // cout<<target_string<<endl;
+    target_string += " 0 0 0 1";
+    vector<double> pose_vec;
+    // stringstream ss(target_string);
+    ss.clear();
+    ss.str(target_string);
+    while(ss>>d){
+        pose_vec.push_back(d);
+    }
+
+    // cout<<cam2pose_vec.size()<<endl;
+    Mat pose_Mat(pose_vec);
+    pose_Mat = pose_Mat.reshape(1,(4,4));
+
     
-    // double invextrinsicMatrix[4][4]={
-    //     {-0.998463, 5.542730e-02, -0.000316, 1.216353e+03},
-    //     {0.002711, 4.315643e-02, -0.999064, -1.648302e+03},
-    //     {-0.055362, -9.975298e-01, -0.043240, 1.354970e+02},
-    //     {0.000000, 0.000000, 0.000000, 1.000000 }
-    // }
+    cout << pose_Mat << endl;
 
-    // cv::Mat cloudPoints(cv::Size(5,5),CV_8UC1);
 
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////cam2world//////////////////////////////////////
+    Mat rectInv = Rect_Mat.inv();
+    // cout<<pose_Mat.size << endl;
+    // cout<<cam2pose_Mat.channels()<<endl;
+    // Mat cam2world = pose_Mat * cam2pose_Mat;
+    Mat cam2world = (pose_Mat * cam2pose_Mat) * rectInv;
+
+    cout<<cam2world<<endl;
+
+    //////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////world2cam/////////////////////////////////////////////////////
+    Mat cam2world_capped = cam2world.rowRange(0,3).clone();
+    cout<<cam2world_capped<<endl;
+    Mat R = cam2world_capped.colRange(0,3).clone();
+    Mat t = cam2world_capped.col(3).clone();
+    cout<<R.t()<<endl;
+    cout<<t.t()<<endl;
+    
+
+    inFile.close();
 
     return 0;
 }
